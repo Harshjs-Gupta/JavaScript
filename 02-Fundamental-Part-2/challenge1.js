91b529182c3f11cd7987eb7f8d830fa4d6031275
Harsh Gupta <harshgupta88911@gmail.com>
2024-02-08 12:21:05 +0530

    calling function with other function

--- 02-Fundamental-Part-2/challenge1.js
+++ 02-Fundamental-Part-2/challenge1.js
@@ -24,21 +24,28 @@ TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
 
 TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27. */
 
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+"use strict";
 
-const scoreDolphins = calcAverage(85, 54, 41);
-const scoreKoalas = calcAverage(23, 34, 27);
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-// console.log(scoreDolphins);
-// console.log(scoreKoalas);
-const checkWinner = (avgDolphins, avgKoalas) => {
-  if (avgDolphins >= 2 * avgKoalas) {
-    console.log(`Dolphins wins🏆 ${avgDolphins} vs ${avgKoalas}`);
-  } else if (avgKoalas >= 2 * avgDolphins) {
-    console.log(`Koalas wins🏆 ${avgKoalas} vs ${avgDolphins}`);
-  } else {
-    console.log("No team win...🥹");
-  }
-};
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
 
-console.log(checkWinner(scoreDolphins, scoreKoalas));
+// // console.log(scoreDolphins);
+// // console.log(scoreKoalas);
+// const checkWinner = (avgDolphins, avgKoalas) => {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins wins🏆 ${avgDolphins} vs ${avgKoalas}`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas wins🏆 ${avgKoalas} vs ${avgDolphins}`);
+//   } else {
+//     console.log("No team win...🥹");
+//   }
+// };
+
+// console.log(checkWinner(scoreDolphins, scoreKoalas));
+
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+
+// console.log(checkWinner(scoreDolphins, scoreKoalas));
