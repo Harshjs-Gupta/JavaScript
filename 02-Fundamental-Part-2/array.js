87209b3aa628c2bee3e16999b7cd3d4cdb233047
Harsh Gupta <harshgupta88911@gmail.com>
2024-02-10 22:06:35 +0530

    output years using index

--- 02-Fundamental-Part-2/array.js
+++ 02-Fundamental-Part-2/array.js
@@ -1,10 +1,11 @@
 "use strict";
 
-const friends = ["Eich", "John", "Bob"];
+const friends = ["Michael", "Steven", "Peter"];
 console.log(friends);
 
-const years = new Array(1991, 1994, 2008, 2020);
+const years = new Array(1991, 1994, 2008, 2020, 2024);
 console.log(years);
+console.log(years[0], years[2], years[4]);
 
 console.log(friends[0], friends[2]);
 
