f8573da8d9c50e35dd714121c17b218f398ed7c4
Harsh Gupta <harshgupta88911@gmail.com>
2024-02-11 11:01:39 +0530

    change value for test case

--- 02-Fundamental-Part-2/array.js
+++ 02-Fundamental-Part-2/array.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const friends = ["Michael", "Steven", "Peter"];
-console.log(friends);
+// const friends = ["Michael", "Steven", "Peter"];
+// console.log(friends);
 
-const years = new Array(1991, 1994, 2008, 2020, "2024");
-console.log(years);
-console.log(years[0], years[2], years[4]);
+// const years = new Array(1991, 1994, 2008, 2020, 2024);
+// console.log(years);
+// console.log(years[0], years[2], years[4]);
 
-console.log(friends[0], friends[2]);
+// console.log(friends[0], friends[2]);
 
-console.log(friends.length);
+// console.log(friends.length);
 
-friends[2] = "jay";
-console.log(friends);
+// friends[2] = "jay";
+// console.log(friends);
