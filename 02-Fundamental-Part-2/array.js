b078d5f7facdbceaaec3e1c85d36b042997cd1ae
Harsh Gupta <harshgupta88911@gmail.com>
2024-02-13 11:51:45 +0530

    commit all code for write a new code

--- 02-Fundamental-Part-2/array.js
+++ 02-Fundamental-Part-2/array.js
@@ -14,27 +14,27 @@ const friends = ["Michael", "Steven", "Peter"];
 // friends[2] = "jay";
 // console.log(friends);
 
-const firstName = "Harsh";
-const harsh = [firstName, "Gupta", "age", 2020 - 2002, friends];
+// const firstName = "Harsh";
+// const harsh = [firstName, "Gupta", "age", 2020 - 2002, friends];
 
-console.log(harsh);
+// console.log(harsh);
 
-const calcAge = function (birthYear) {
-  return 2030 - birthYear;
-};
+// const calcAge = function (birthYear) {
+//   return 2030 - birthYear;
+// };
 
-const year = [1990, 1967, 2008, 2018];
+// const year = [1990, 1967, 2008, 2018];
 
-const age1 = calcAge(year[0]);
-const age2 = calcAge(year[1]);
-const age3 = calcAge(year[year.length - 1]);
+// const age1 = calcAge(year[0]);
+// const age2 = calcAge(year[1]);
+// const age3 = calcAge(year[year.length - 1]);
 
-console.log(age1, age2, age3);
+// console.log(age1, age2, age3);
 
-const ages = [
-  calcAge(year[0]),
-  calcAge(year[1]),
-  calcAge(year[year.length - 1]),
-];
+// const ages = [
+//   calcAge(year[0]),
+//   calcAge(year[1]),
+//   calcAge(year[year.length - 1]),
+// ];
 
-console.log(ages);
+// console.log(ages);
