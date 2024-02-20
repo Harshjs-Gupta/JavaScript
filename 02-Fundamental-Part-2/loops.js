69469e04789f1f0092676dd0f349fb8413c174ce
Harsh Gupta <harshgupta88911@gmail.com>
2024-02-20 19:01:14 +0530

    run a for loop for printing array elements

--- 02-Fundamental-Part-2/loops.js
+++ 02-Fundamental-Part-2/loops.js
@@ -1,6 +1,20 @@
 "use strict";
 /* here we know how to write for loop in javascript*/
 
-for (let i = 1; i <= 10; i++) {
-  console.log(`Lifting weights repetition ${i} 🏋️`);
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`Lifting weights repetition ${i} 🏋️`);
+// }
+
+/* Looping Arrays, Breaking and continuing*/
+
+const harsh = [
+  "harsh",
+  "gupta",
+  2030 - 2019,
+  "student ",
+  ["Michale", "Peter", "Steven"],
+];
+
+for (let i = 0; i < 5; i++) {
+  console.log(harsh[i]);
 }
