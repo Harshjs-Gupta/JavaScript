f43fc0b863e6a6bdcbf28f2d1df6f08afe14aa54
Harsh Gupta <harshgupta88911@gmail.com>
2024-02-20 20:15:20 +0530

    here we know about how continue statement work in loop

--- 02-Fundamental-Part-2/loops.js
+++ 02-Fundamental-Part-2/loops.js
@@ -7,26 +7,42 @@
 
 /* Looping Arrays, Breaking and continuing*/
 
+// const harsh = [
+//   "harsh",
+//   "gupta",
+//   2030 - 2019,
+//   "student ",
+//   ["Michale", "Peter", "Steven"],
+// ];
+
+// const type = [];
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(harsh[i]);
+
+//   type.push(typeof harsh[i]);
+// }
+// console.log(type);
+
+// const years = [1991, 2007, 1969, 2020];
+// const age = [];
+// for (let i = 0; i < years.length; i++) {
+//   age.push(2037 - years[i]);
+// }
+// console.log(age);
+
+//Continue and break statement
 const harsh = [
-  "harsh",
-  "gupta",
+  "Harsh",
+  "Gupta",
   2030 - 2019,
   "student ",
   ["Michale", "Peter", "Steven"],
 ];
 
-const type = [];
-
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < harsh.length; i++) {
+  if (typeof harsh[i] !== "string") {
+    continue;
+  }
   console.log(harsh[i]);
-
-  type.push(typeof harsh[i]);
 }
-console.log(type);
-
-const years = [1991, 2007, 1969, 2020];
-const age = [];
-for (let i = 0; i < years.length; i++) {
-  age.push(2037 - years[i]);
-}
-console.log(age);
