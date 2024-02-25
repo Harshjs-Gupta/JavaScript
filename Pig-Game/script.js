bf35ced0b5da86da0d1d7e00d0d68d8a11a10de1
Harsh Gupta <harshgupta88911@gmail.com>
2024-02-25 13:46:09 +0530

    add html , css and js file for pig game

---
+++ Pig-Game/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const Player0El = document.querySelector(".player--0");
+const Player1El = document.querySelector(".player--1");
+const Score0El = document.querySelector("#score--0");
+const Score1El = document.getElementById("score--1");
+const Current0El = document.getElementById("current--0");
+const Current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn-hold");
+
+btnNEw.addEventListener("click", function () {});
