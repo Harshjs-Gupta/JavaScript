61fb660a7743f0cd5046056bfbfe59993715dc1c
Harsh Gupta <harshgupta88911@gmail.com>
2024-03-05 12:56:00 +0530

    here we know the work of classList.remove function

--- 07-DomAndEventManipulation/workingWithClass.js
+++ 07-DomAndEventManipulation/workingWithClass.js
@@ -1,2 +1,4 @@
-//this classList.add is use to add class attributes in html.
+//this classList.add is use to add class attributes in html and their works
 document.querySelector("hidden").classList.add("hidden");
+//this classList.remove is use to remove class attributes in html and their works
+document.querySelector("hidden").classList.remove("hidden");
