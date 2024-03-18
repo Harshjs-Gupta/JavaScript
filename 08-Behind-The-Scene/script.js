5642a0a9223499fbd38e1b27908bbbfd43bf71ad
Harsh Gupta <harshgupta88911@gmail.com>
2024-03-18 19:18:50 +0530

    here we know scoping

--- 08-Behind-The-Scene/script.js
+++ 08-Behind-The-Scene/script.js
@@ -1,15 +1,19 @@
 "use strict";
 
-function calcAge(birthyear) {
-  const age = 2037 - birthyear;
+/* function calcAge(birthyear) {
+  const age = 2024 - birthyear;
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthyear}`;
+    const output = `${firstName}, You are ${age}, born in ${birthyear}`;
     console.log(output);
+    if (birthyear >= 2000 && birthyear <= 2013) {
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
   }
-
   printAge();
+
   return age;
 }
 const firstName = "Harsh";
-calcAge(1991);
+calcAge(2004); */
