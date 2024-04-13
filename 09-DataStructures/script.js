83604912b7f5474ec2d778a0ad107ee1c64e9beb
Harsh Gupta <harshgupta88911@gmail.com>
2024-04-13 22:12:18 +0530

    here we know about slice() Method

--- 09-DataStructures/script.js
+++ 09-DataStructures/script.js
@@ -490,3 +490,25 @@ console.log(main, secondary);
 // console.log(airline.indexOf("r"));
 // console.log(airline.lastIndexOf("r"));
 // console.log(airline.indexOf("Portugal"));
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline.slice(0, airline.indexOf(" ")));
+// console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
+// const checkMiddleSeat = function (seat) {
+//   // B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === "B" || s === "E") console.log("You got the middle seat 😬");
+//   else console.log("You got lucky 😎");
+// };
+
+// checkMiddleSeat("11B");
+// checkMiddleSeat("23C");
+// checkMiddleSeat("3E");
+// console.log(new String("jonas"));
+// console.log(typeof new String("jonas"));
+// console.log(typeof new String("jonas").slice(1));
