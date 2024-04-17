a5a944ce39dfc47e9ed9451d922734bd522eba85
Harsh Gupta <harshgupta88911@gmail.com>
2024-04-17 15:08:09 +0530

    here we know about function

--- 10-Function/script.js
+++ 10-Function/script.js
@@ -38,3 +38,19 @@ const upperFirstWord = function (str) {
   const [first, ...others] = str.split(" ");
   return [first.toUpperCase(), ...others].join(" ");
 };
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log("👋");
+};
+document.body.addEventListener("click", high5);
