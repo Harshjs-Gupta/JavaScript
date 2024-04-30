8a35054452e8911b50af044c0ec54b17fdb0da11
Harsh Gupta <harshgupta88911@gmail.com>
2024-04-30 18:56:39 +0530

    Here we practice more example of closure

--- 10-Function/script.js
+++ 10-Function/script.js
@@ -221,13 +221,48 @@
 
 ///////////////////////////////////
 /* Closures */
-const secureBooking = function () {
-  let passengerCount = 0;
+// const secureBooking = function () {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+/////////////////////////////
+/* More Closure Examples */
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
   };
 };
 
-const booker = secureBooking();
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
