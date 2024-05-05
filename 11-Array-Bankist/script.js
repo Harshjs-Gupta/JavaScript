48910eb69309a974d7948dd190fe208db126a74c
Harsh Gupta <harshgupta88911@gmail.com>
2024-05-05 13:30:03 +0530

    here we print only negative number in movements

--- 11-Array-Bankist/script.js
+++ 11-Array-Bankist/script.js
@@ -140,13 +140,16 @@
 /* The filter Method */
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const deposits = movements.filter(function (mov) {
-  return mov > 0;
-});
-console.log(movements);
-console.log(deposits);
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+// console.log(movements);
+// console.log(deposits);
 
-const depositsFor = [];
-for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
 
-console.log(depositsFor);
+// console.log(depositsFor);
+
+const withdrawal = movements.filter((mov) => mov < 0);
+console.log(withdrawal);
