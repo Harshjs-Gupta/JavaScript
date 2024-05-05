459b146fe339fd2a52408a37de91c2e7cd1737d4
Harsh Gupta <harshgupta88911@gmail.com>
2024-05-05 19:12:22 +0530

    here we do same thing but using for of loop

--- 11-Array-Bankist/script.js
+++ 11-Array-Bankist/script.js
@@ -166,3 +166,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // Arrow function
 const balance = movements.reduce((acc, cur) => acc + cur, 0);
 console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
