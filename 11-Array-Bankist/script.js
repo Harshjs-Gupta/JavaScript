50333226ebf01f150cf88760e0838228ab6d09bc
Harsh Gupta <harshgupta88911@gmail.com>
2024-05-09 12:37:19 +0530

    here we know about how flat method is work

--- 11-Array-Bankist/script.js
+++ 11-Array-Bankist/script.js
@@ -419,3 +419,19 @@ btnClose.addEventListener("click", function (e) {
 //EVERY: CONDITION
 // console.log(movements.every((mov) => mov > 0));
 // console.log(account4.movements.every((mov) => mov > 0));
+
+/////////////////////////////////////////
+/* flat and flatMap */
+// const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+// console.log(arr.flat());
+
+// const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+// console.log(arrDeep.flat(2));
+
+// //flat
+// const overallBalance = accounts
+//   .map((acc) => acc.movements)
+//   .flat()
+//   .reduce((acc, mov) => acc + mov, 0);
+
+// console.log(overallBalance);
