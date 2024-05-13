005ed355321fd46e9efcecebc61cd318d4fb3613
Harsh Gupta <harshgupta88911@gmail.com>
2024-05-13 20:20:12 +0530

    here we check fi value is number or not by using isFinite() Method

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -394,3 +394,13 @@ btnSort.addEventListener("click", function (e) {
 // console.log(Number.isNaN('20'));
 // console.log(Number.isNaN(+'20X'));
 // console.log(Number.isNaN(23 / 0));
+
+/* Checking if value is number */
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(+'20X'));
+// console.log(Number.isFinite(23 / 0));
+
+// console.log(Number.isInteger(23));
+// console.log(Number.isInteger(23.0));
+// console.log(Number.isInteger(23 / 0));
