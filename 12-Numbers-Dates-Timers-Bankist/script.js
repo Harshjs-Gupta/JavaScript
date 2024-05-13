1b2d52787cfc9d4239674d10d17026f7d1877d0e
Harsh Gupta <harshgupta88911@gmail.com>
2024-05-13 15:46:42 +0530

    here we kanow about a function which is parseInt()

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -379,3 +379,12 @@ btnSort.addEventListener("click", function (e) {
 // /* Conversion */
 // console.log(Number('23'));
 // console.log(+'23');
+
+// /* Parsing */
+// console.log(Number.parseInt('30px', 10));
+// console.log(Number.parseInt('e23', 10));
+
+// console.log(Number.parseInt('  2.5rem  '));
+// console.log(Number.parseFloat('  2.5rem  '));
+
+// console.log(parseFloat('  2.5rem  '));
