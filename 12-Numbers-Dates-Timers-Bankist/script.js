6d79bdee49903579f28a0a6893429c67e281e644
Harsh Gupta <harshgupta88911@gmail.com>
2024-05-16 12:27:15 +0530

    here we work with SetTimeout

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -571,3 +571,17 @@ btnSort.addEventListener("click", function (e) {
 //   navigator.language,
 //   new Intl.NumberFormat(navigator.language, option).format(num)
 // );
+
+///////////////////////////////////////
+/* Timers : setTimeout and setInterval */
+// //SetTimeout
+// const ingredients = ["olives", "spinach"];
+// const pizzaTimer = setTimeout(
+//   (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+//   3000,
+//   ...ingredients
+// );
+// console.log("Waiting...");
+// if (ingredients.includes("spinach")) {
+//   clearTimeout(pizzaTimer);
+// }
