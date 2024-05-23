8e00173a2333d803ac8df2d4f4b6e56075a2e6b3
Harsh Gupta <harshgupta88911@gmail.com>
2024-05-23 16:21:51 +0530

    here we know about push method

---
+++ 11-Array-Bankist/allArrayMethods.js
@@ -0,0 +1,11 @@
+"use strict";
+/* Add element an array */
+/* 1st Method: push() method add one or more elements at the end of an array*/
+/* Example 1 add one element */
+// const arr = ["bob", "steven", "john", "sam", "archi"];
+// arr.push("samridhi");
+// console.log(arr);
+
+/* Example 2 add one or more elements */
+// arr.push("samridhi","daivd");
+// console.log(arr);
