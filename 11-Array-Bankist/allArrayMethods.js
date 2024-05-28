676424b850852373b30de477504a1361e6a46276
Harsh Gupta <harshgupta88911@gmail.com>
2024-05-28 13:22:26 +0530

    here we know about flatMap

--- 11-Array-Bankist/allArrayMethods.js
+++ 11-Array-Bankist/allArrayMethods.js
@@ -125,3 +125,19 @@
 // ];
 // const output = arr.flat(2);
 // console.log(output);
+
+/* 14th Method: flatMap() method can be used as a way to add and remove items (modify the number fo items) during a map it takes three parameter current value , index, array*/
+/* Example 1 */
+// Let's say we want to remove all the negative numbers
+// and split the odd numbers into an even number and a 1
+// const a = [5, 4, -3, 20, 17, -33, -4, 18];
+// //         |\  \  x   |  | \   x   x   |
+// //        [4,1, 4,   20, 16, 1,       18]
+
+// const result = a.flatMap((n) => {
+//   if (n < 0) {
+//     return [];
+//   }
+//   return n % 2 === 0 ? [n] : [n - 1, 1];
+// });
+// console.log(result); // [4, 1, 4, 20, 16, 1, 18]
