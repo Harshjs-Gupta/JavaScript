dec22b055f287aefff4f94eb9cf45b07a57ef2f6
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-02 22:43:27 +0530

    here we uncommit all code

--- 11-Array-Bankist/test.js
+++ 11-Array-Bankist/test.js
@@ -1,21 +1,21 @@
-// const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// const output = array.map((curr) => {
-//   if (curr % 2 === 0) {
-//     return curr * 2;
-//   } else {
-//     return curr * 3;
-//   }
-// });
-// console.log(output);
-// const output2 = output.filter((val) => val % 2 === 0);
-// console.log(output2);
-// // const output3 = output2.sort((val1, val2) => val1 < val2);
-// // console.log(output3);
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const output = array.map((curr) => {
+  if (curr % 2 === 0) {
+    return curr * 2;
+  } else {
+    return curr * 3;
+  }
+});
+console.log(output);
+const output2 = output.filter((val) => val % 2 === 0);
+console.log(output2);
+// const output3 = output2.sort((val1, val2) => val1 < val2);
+// console.log(output3);
 
-// // const num1 = "2";
-// // const num2 = 2;
-// // console.log(num1 === num2);
+// const num1 = "2";
+// const num2 = 2;
+// console.log(num1 === num2);
 
-// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// const output4 = arr.reduce((acc, curr) => (curr % 2 === 0 ? acc + curr : acc));
-// console.log(output4);
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const output4 = arr.reduce((acc, curr) => (curr % 2 === 0 ? acc + curr : acc));
+console.log(output4);
