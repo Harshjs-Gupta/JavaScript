2d51743f484307a27020a9bdbcf1c154f65c9861
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-07 19:02:45 +0530

    here we add eventListener to an link

--- 13-Advance-DOM-Bankist/script.js
+++ 13-Advance-DOM-Bankist/script.js
@@ -78,3 +78,15 @@ document.querySelectorAll(".nav__link").forEach(function (el) {
 });
 
 ////////////////////////////////////////////
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
