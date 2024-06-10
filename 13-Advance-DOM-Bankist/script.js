a686af83205caf87b138861cbedbcb7caeb76ccc
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-10 14:29:25 +0530

    here we know about creating and inserting an element

--- 13-Advance-DOM-Bankist/script.js
+++ 13-Advance-DOM-Bankist/script.js
@@ -310,3 +310,17 @@ slider();
 // console.log(allButtons);
 
 // console.log(document.getElementsByClassName('btn'));
+
+// // Creating and inserting elements
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// // message.textContent = 'We use cookied for improved functionality and analytics.';
+// message.innerHTML =
+// 'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// // header.prepend(message);
+// header.append(message);
+// // header.append(message.cloneNode(true));
+
+// // header.before(message);
+// // header.after(message);
