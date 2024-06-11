848ee5cdf2f24de8c7e4b0e9355e207f04a5b635
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-11 18:15:45 +0530

    here we know about type of event listeners

--- 13-Advance-DOM-Bankist/script.js
+++ 13-Advance-DOM-Bankist/script.js
@@ -381,3 +381,13 @@ slider();
 
 // // Don't use
 // logo.clasName = 'jonas';
+
+///////////////////////////////////////
+/* Types of Events and Event Handlers */
+// const h1 = document.querySelector('h1');
+
+// const alertH1 = function (e) {
+// alert('addEventListener: Great! You are reading the heading :D');
+// };
+
+// h1.addEventListener('mouseenter', alertH1);
