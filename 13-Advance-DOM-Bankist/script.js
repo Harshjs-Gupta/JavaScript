e72065b46d55105c6e9160b97fce6e80ae23c7cf
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-13 17:37:52 +0530

    here we add event propagation in nav link button

--- 13-Advance-DOM-Bankist/script.js
+++ 13-Advance-DOM-Bankist/script.js
@@ -397,3 +397,15 @@ slider();
 // // h1.onmouseenter = function (e) {
 // //   alert('onmouseenter: Great! You are reading the heading :D');
 // // };
+
+///////////////////////////////////////
+/* Event Propagation in Practice */
+// const randomInt = (min, max) =>
+// Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+// `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+// this.style.backgroundColor = randomColor();
+// console.log('LINK', e.target, e.currentTarget);
+// console.log(e.currentTarget === this);
