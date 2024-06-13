3a2ad1c0a920ee325a2926d1ed5a0f1ce10194d1
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-13 17:40:53 +0530

    here we add event propagation on nav links and nav also

--- 13-Advance-DOM-Bankist/script.js
+++ 13-Advance-DOM-Bankist/script.js
@@ -409,3 +409,17 @@ slider();
 // this.style.backgroundColor = randomColor();
 // console.log('LINK', e.target, e.currentTarget);
 // console.log(e.currentTarget === this);
+
+// // Stop propagation
+// // e.stopPropagation();
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+// this.style.backgroundColor = randomColor();
+// console.log('CONTAINER', e.target, e.currentTarget);
+// });
+
+// document.querySelector('.nav').addEventListener('click', function (e) {
+// this.style.backgroundColor = randomColor();
+// console.log('NAV', e.target, e.currentTarget);
+// });
