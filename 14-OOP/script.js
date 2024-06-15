9b3dc8a665ee071d1f7fd2fe91b3fca0b055cc10
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-15 13:11:56 +0530

    here we know how to call constructor function

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -14,3 +14,17 @@
 
 // const harsh = new Person("Harsh", 1991);
 // console.log(harsh);
+
+// // 1. New {} is created
+// // 2. function is called, this = {}
+// // 3. {} linked to prototype
+// // 4. function automatically return {}
+
+// const matilda = new Person("matilda", 2017);
+// const jack = new Person("Jack", 1975);
+// console.log(matilda, jack);
+
+// const jay = Person("Jay", 2016);
+// // USE of instanceof Method: Here we check whether an object belongs to a certain class or not
+// console.log(harsh instanceof Person); //true
+// console.log(jay instanceof Person); //false
