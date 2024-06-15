0152d7815ce7e78ce489ea27749463eaef7b8dee
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-15 13:10:49 +0530

    here we know about constructor function

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,16 @@
-"use strict";
+/* LECTURE 209 */
+/* Constructor function and the new operator */
+
+// const Person = function (firstName, birthyear) {
+//   //Instance properties
+//   this.firstName = firstName;
+//   this.birthyear = birthyear;
+
+//   //Never do this
+//   // this.calcAge = function(){
+//   //   console.log(2037 - this.birthyear);
+//   // }
+// };
+
+// const harsh = new Person("Harsh", 1991);
+// console.log(harsh);
