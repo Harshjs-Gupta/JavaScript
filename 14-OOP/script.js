524ad202368ad2eae9d57dc3f6e461ad47f8a506
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-19 19:07:18 +0530

    here we know about how to wirte class

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -76,3 +76,49 @@
 
 // const h1 = document.querySelector('h1');
 // console.dir(x => x + 1);
+
+///////////////////////////////////////
+// ES6 Classes
+
+// Class expression
+// const PersonCl = class {}
+
+// Class declaration
+// class PersonCl {
+//     constructor(fullName, birthYear) {
+//       this.fullName = fullName;
+//       this.birthYear = birthYear;
+//     }
+
+//     // Instance methods
+//     // Methods will be added to .prototype property
+//     calcAge() {
+//       console.log(2037 - this.birthYear);
+//     }
+
+//     greet() {
+//       console.log(`Hey ${this.fullName}`);
+//     }
+
+//     get age() {
+//       return 2037 - this.birthYear;
+//     }
+
+//     // Set a property that already exists
+//     set fullName(name) {
+//       if (name.includes(' ')) this._fullName = name;
+//       else alert(`${name} is not a full name!`);
+//     }
+
+//     get fullName() {
+//       return this._fullName;
+//     }
+
+//     // Static method
+//     static hey() {
+//       console.log('Hey there 👋');
+//       console.log(this);
+//     }
+//   }
+
+//   const jessica = new PersonCl('Jessica Davis', 1996);
