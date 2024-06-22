0d596cd0f258b31824c37ded53114f3cc0d9884d
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-22 12:44:45 +0530

    here we know how to inherit object from constructor function

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -185,3 +185,15 @@
 //   const sarah = Object.create(PersonProto);
 //   sarah.init('Sarah', 1979);
 //   sarah.calcAge();
+
+///////////////////////////////////////
+// Inheritance Between "Classes": Constructor Functions
+
+// const Person = function (firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   };
+
+//   Person.prototype.calcAge = function () {
+//     console.log(2037 - this.birthYear);
+//   };
