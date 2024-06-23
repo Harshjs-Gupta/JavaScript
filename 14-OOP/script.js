44f073566b05c7c3930d93cc1320d3a7bb67143f
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-23 20:20:04 +0530

    here we check instance of mike

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -216,3 +216,10 @@
 
 //   console.log(mike.__proto__);
 //   console.log(mike.__proto__.__proto__);
+
+//   console.log(mike instanceof Student);
+//   console.log(mike instanceof Person);
+//   console.log(mike instanceof Object);
+
+//   Student.prototype.constructor = Student;
+//   console.dir(Student.prototype.constructor);
