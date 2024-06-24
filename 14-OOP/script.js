d521eedd4210379c524a0dcbda3d551331bc06b1
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-24 12:20:12 +0530

    here we add introduce function in child class

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -308,3 +308,16 @@
 //   PersonProto.init.call(this, firstName, birthYear);
 //   this.course = course;
 // };
+
+// StudentProto.introduce = function () {
+//   // BUG in video:
+//   // console.log(`My name is ${this.fullName} and I study ${this.course}`);
+
+//   // FIX:
+//   console.log(`My name is ${this.firstName} and I study ${this.course}`);
+// };
+
+// const jay = Object.create(StudentProto);
+// jay.init("Jay", 2010, "Computer Science");
+// jay.introduce();
+// jay.calcAge();
