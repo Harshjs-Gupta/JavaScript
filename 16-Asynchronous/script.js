6bee74567c8b42f2d8c2311d098a8298ca605db9
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-26 19:53:14 +0530

    here we add html  in countryContainer

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -2,3 +2,29 @@
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
+
+const renderCountry = function (data, className = "") {
+  const currencyKey = Object.keys(data.currencies);
+  // console.log(currencyKey);
+  const currency = data.currencies[currencyKey];
+  // console.log(currency.symbol);
+  const lang = Object.values(data.languages).join(", ");
+  // console.log(lang);
+
+  const html = `<article class="country ${className}">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name"> ${data.name.common} </h3>
+            <h4 class="country__region"> ${data.region} </h4>
+            <p class="country__row"><span>👫</span> ${(
+              +data.population / 1000000
+            ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span> ${lang} </p>
+            <p class="country__row"><span>💰</span> ${currency.name} (${
+    currency.symbol
+  }) </p>
+          </div>
+        </article>`;
+
+  countriesContainer.insertAdjacentHTML("beforebegin", html);
+};
