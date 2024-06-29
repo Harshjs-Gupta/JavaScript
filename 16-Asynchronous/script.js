f332d2e902fd83f8fe4a5c4b5b67f6a8425da715
Harsh Gupta <harshgupta88911@gmail.com>
2024-06-29 12:56:42 +0530

    here we doing AJAX call using XMLHttpRequest

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -45,3 +45,47 @@ const getJSON = function (url, errorMSg = "Something went wrong") {
     return response.json();
   });
 };
+
+///////////////////////////////////////
+/* LECTURE 249 */
+/* Our First AJAX Call: XMLHttpRequest */
+
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+//   request.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     // const currencyKey = Object.keys(data.currencies)[0];
+//     // // console.log(currencyKey);
+//     // const currency = data.currencies[currencyKey];
+//     // // console.log(currency.symbol);
+//     // const lang = Object.values(data.languages).join(", ");
+//     // // console.log(lang);
+
+//     // const html = `<article class="country">
+//     //       <img class="country__img" src="${data.flags.png}" />
+//     //       <div class="country__data">
+//     //         <h3 class="country__name"> ${data.name.common} </h3>
+//     //         <h4 class="country__region"> ${data.region} </h4>
+//     //         <p class="country__row"><span>👫</span> ${(
+//     //           +data.population / 1000000
+//     //         ).toFixed(1)}M people</p>
+//     //         <p class="country__row"><span>🗣️</span> ${lang} </p>
+//     //         <p class="country__row"><span>💰</span> ${currency.name} (${
+//     //   currency.symbol
+//     // }) </p>
+//     //       </div>
+//     //     </article>`;
+
+//     // countriesContainer.insertAdjacentHTML("beforebegin", html);
+
+//     renderCountry(data);
+//   });
+// };
+
+// getCountryData("bharat");
+// getCountryData("usa");
