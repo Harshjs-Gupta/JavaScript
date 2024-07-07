6f42752360a7db6a718bf0e33d5c0744f625f7c5
Harsh Gupta <harshgupta88911@gmail.com>
2024-07-07 13:31:28 +0530

    here we efficient code to get country data

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -240,3 +240,42 @@ const getJSON = function (url, errorMSg = "Something went wrong") {
     })
     .finally(() => (countriesContainer.style.opacity = 1));
 }; */
+
+/* Efficient coded */
+// const getCountryData = function (country) {
+//   //country 1
+//   getJSON(
+//     `https://restcountries.com/v3.1/name/${country}`,
+//     "Country not found!"
+//   )
+//     .then((data) => {
+//       // console.log(data);
+//       renderCountry(data[0]);
+
+//       const neighbour = data[0].borders ? data[0].borders[0] : undefined;
+
+//       if (!neighbour) throw new Error("No neighbour found!");
+//       // else {
+//       //   getNeighbourData(neighbour);
+//       // }
+
+//       //country 2
+//       return getJSON(
+//         `https://restcountries.com/v3.1/alpha/${neighbour}`,
+//         "Country not found!"
+//       );
+//     })
+//     .then((data) => {
+//       // console.log(data);
+//       renderCountry(data[0], "neighbour");
+//     })
+//     .catch((err) => {
+//       console.error(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+//     })
+//     .finally(() => (countriesContainer.style.opacity = 1));
+// };
+
+// btn.addEventListener("click", function () {
+//   getCountryData("bharat");
+// });
