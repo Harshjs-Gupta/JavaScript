570800352de3ded1f6c943eff8ffff838d41c9ac
Harsh Gupta <harshgupta88911@gmail.com>
2024-07-07 13:24:15 +0530

    how to chaining promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -174,3 +174,22 @@ const getJSON = function (url, errorMSg = "Something went wrong") {
 
 // getCountryData("bharat");
 // getCountryData("usa");
+
+//////////////////////////////////////////
+/* LECTURE 254 */
+/* Chaining Promises */
+
+// const getNeighbourData = (neighbour) => {
+//   getJSON(
+//     `https://restcountries.com/v3.1/alpha/${neighbour}`,
+//     "Country not found!"
+//   )
+//     .then((data) => {
+//       // console.log(data);
+//       renderCountry(data[0], "neighbour");
+//     })
+//     .catch((err) => {
+//       console.error(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+//     });
+// };
