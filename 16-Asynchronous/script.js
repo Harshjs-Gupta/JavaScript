a91f81048df4e83372b8ac4a936e15b3a93af028
Harsh Gupta <harshgupta88911@gmail.com>
2024-07-09 13:00:22 +0530

    here we also use .then keyword in place of callback hell

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -380,3 +380,32 @@ TEST COORDINATES 2: -33.933, 18.474 */
 //     return wait(1);
 //   })
 //   .then(() => console.log("I wait for 1 second"));
+
+// setTimeout(() => {
+//   console.log("1 second passed");
+//   setTimeout(() => {
+//     console.log("2 second passed");
+//     setTimeout(() => {
+//       console.log("3 second passed");
+//       setTimeout(() => {
+//         console.log("4 second passed");
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+/* Instead of this ☝️ write like this 👇 */
+// wait(1)
+//   .then(() => {
+//     console.log("1 second passed");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     console.log("2 second passed");
+//     return wait(3);
+//   })
+//   .then(() => {
+//     console.log("3 second passed");
+//     return wait(4);
+//   })
+//   .then(() => console.log("4 second passed"));
